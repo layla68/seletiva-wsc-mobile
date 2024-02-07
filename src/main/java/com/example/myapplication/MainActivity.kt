9b373811c9b8a9567d7dc9404b67b012fa95293a
layla68 <82838464+layla68@users.noreply.github.com>
2024-02-07 12:08:49 -0300

    Add files via upload

---
+++ src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.imageView2.setOnClickListener {
+            val navegarInicial = Intent(this, TelaInicial :: class.java)
+            startActivity(navegarInicial)
+        }
+
+    }
+
+
+}
